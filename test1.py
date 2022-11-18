eba93694a463de7c666b12534a963a34eabee70d
klay1001 <arthur40901@gmail.com>
2022-11-18 15:53:43 +0800

    Third Commit

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
 import math
 import random
 from scipy.stats import norm, zscore
-population=eval(input("有幾個同學: "))
+population=eval(input("有幾個同學呢: "))
 data = range(1,population+1)
 print("請回答問題: ",random.sample(data,1))
