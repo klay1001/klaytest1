6d2e1cb11b46de4c46fdeb2efb9c38f95cfea924
klay1001 <arthur40901@gmail.com>
2022-11-18 15:23:06 +0800

    Second Commit

--- test1.py
+++ test1.py
@@ -3,4 +3,4 @@ import random
 from scipy.stats import norm, zscore
 population=eval(input("有幾個同學: "))
 data = range(1,population+1)
-print("請回答: ",random.sample(data,1))
+print("請回答問題: ",random.sample(data,1))
